b167f9c924ab44bd8eb3d83f32b10618bff5dc4f
Lars Funke <lars.funke@udo.edu>
2020-11-19 17:59:16 +0100

    Only go up to sqrt(n)+1

--- 1_1.py
+++ 1_1.py
@@ -7,7 +7,7 @@ import numpy as np
 text = "Hello DELTA!"
 print(text)
 # The `with` statement automatically closes the file after writing is done.
-with open("build/file1.txt", "w") as f:
+with open("build/1_1/file1.txt", "w") as f:
     f.write(text)
 
 
@@ -21,30 +21,30 @@ print(array)
 # np.savetxt saves a numpy array to a text file in a human-readable format.
 # The fmt argument specifies the number formatting, as in general
 # there are many ways of representing a number in text.
-np.savetxt("build/file2.txt", array, fmt="%i")
+np.savetxt("build/1_1/file2.txt", array, fmt="%i")
 
 
 ######
 # c) #
 ######
-# This so-called “decorator” allows this function to be applied to a numpy array.
+# This “decorator” allows this function to be applied to a numpy array.
 @np.vectorize
 # The function isprime therefore only needs to check the prime-ness of one number.
 def isprime(a):
-    # A number is a prime when it is larger than one and all division remainders up
-    # to the number itself are non-zero
-    return a > 1 and all(a % i for i in range(2, a))
-    #                    ^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # This is called a “list comprehension” and is an short and easy way to generate
-    # a list in python. It contains all remainders up to the number being checked.
+    # A number is a prime if it is larger than one and all division remainders up
+    # to half of the number itself are non-zero
+    return a > 1 and all(a % i for i in range(2, int(np.sqrt(a)) + 1))
+    #                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    # This is called a “list comprehension” and is a short and easy way to generate
+    # a list in python. The list contains all remainders up to the number being checked.
     #
     # The `all` function returns False if any of the numbers in the are zero, else True.
 
 
 # np.genfromtxt allows to read data from a human-readable file like the one we saved before.
-numbers = np.genfromtxt("build/file2.txt", dtype=int)
+numbers = np.genfromtxt("build/1_1/file2.txt", dtype=int)
 print(numbers[isprime(numbers)])
 #             ^^^^^^^^^^^^^^^^
-# The return value if this is called a mask. It contains a True/False value for every
-# item in the list it refers to. When you execute `array[mask]`, you get back only
+# The return value of this method is called a mask. It contains a True/False value for
+# every item in the list it refers to. When you execute `array[mask]`, you get back only
 # the list items that had True as their mask entry.
