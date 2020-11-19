ab8782b4381efc4ecd5685f2cf4a9ccdeb3c0d0b
Lars Funke <lars.funke@udo.edu>
2020-11-19 17:59:28 +0100

    Added sheet 2

---
+++ 1_2.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import scipy.constants as const
+from mpl_toolkits.mplot3d import Axes3D
+
+######
+# a) #
+######
+# Load electron mass from scipy constants, then divide by 1000 to convert to GeV/c^2
+m_e = const.value('electron mass energy equivalent in MeV') * 1e-3
+
+# Create 200 equidistant points from 511keV to 6GeV
+E = np.linspace(511e-6, 6, 150)
+# Meshgrid takes the 1D energy points and creates a 2D grid (with a size of 150×150)
+E1, E2 = np.meshgrid(E, E)
+# In a head-on collision, the invariant mass formula is simple:
+M = np.sqrt(2 * m_e**2 + 2 * (E1 * E2 + np.sqrt(E1**2 - m_e**2) * np.sqrt(E2**2 - m_e**2)))
+
+
+######
+# b) #
+######
+# Plot in 1D for E_2 = 0 (takes just the first row of the grid)
+plt.plot(E, M[0])
+# You can use some LaTeX commands in labels if you enclose them in $…$
+# The string prefix `r` means these are “raw strings”, normally python would interpret
+# everything starting with a backslash as a control character, now it does not.
+plt.xlabel(r'$E_1$ / GeV')
+plt.ylabel(r'$\sqrt{s}$ / GeV/c$^2$')
+# Set plot limits
+plt.xlim(E[0], E[-1])
+plt.ylim(M[0][0], M[0][-1])
+# `plt.tight_layout` reorders the graphical elements optimally for smaller margins.
+plt.tight_layout(pad=0.5)
+# When you export an image, the `dpi` (dots per inch) parameter sets the resolution.
+# Most of the time, it is better to save plots as PDF files (vector) and not raster images.
+plt.savefig('build/1_2/1d.png', dpi=150)
+# Close the plot so nothing of this shows up in the next ones.
+plt.close()
+
+
+######
+# c) #
+######
+# Plot in 3D
+fig = mpl.figure.Figure()
+ax = Axes3D(fig)
+# The stride parameters allow you to skip values if plotting is too slow.
+# `cmap` chooses a color map for the z value. Good color maps are viridis, plasma and magma.
+ax.plot_surface(E1, E2, M, rstride=1, cstride=1, cmap='viridis')
+# Default is a left-handed coordinate system.
+ax.invert_yaxis()
+ax.set_xlabel(r'$E_1$ / GeV')
+ax.set_ylabel(r'$E_2$ / GeV')
+ax.set_zlabel(r'$\sqrt{s}$ / GeV/c$^2$')
+fig.savefig('build/1_2/3d.png', dpi=150)
+
+# Plot in 2D (optional)
+# A 2D color plot is nothing else than an image. There fore we use `imshow` to make one.
+# Computer images traditionally have their origin on the upper left, we need to change that.
+# `extent` tells matplotlib the size of the image in units of energy for the tick labels.
+# Note, 2 * [a, b] == [a, b, a, b]. We do not need a quadratic image, therefore `aspect='auto'`.
+plt.imshow(M, origin='lower', extent=2 * (E[0], E[-1]), aspect='auto')
+plt.xlabel(r'$E_1$ / GeV')
+plt.ylabel(r'$E_2$ / GeV')
+plt.colorbar(label=r'$\sqrt{s}$ / GeV/c$^2$')
+plt.tight_layout(pad=0.5)
+plt.savefig('build/1_2/2d.png', dpi=150)
+plt.close()
