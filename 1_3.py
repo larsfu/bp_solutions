ab3dcb1593a74a6d747f8a76b2fe7dfcbb7d6eb1
Lars Funke <lars.funke@udo.edu>
2020-12-03 15:34:42 +0100

    small tweaks

--- 1_3.py
+++ 1_3.py
@@ -13,17 +13,17 @@ def set_electrodes(potential, o, w):
 
 # This function takes the current potential array and does one iteration step on it
 def iterate_manual(old):
-    # We need two arrays, old and new, therefore we copy the old one
+    # We need two arrays, old and new values, therefore we copy the old one
     copied = np.copy(old)
 
     # To calculate the averages, the whole field is shifted in four different ways
     # instead of manually looping through the array in two four loops. This is
     # much faster.
 
-    # central nodes
+    # Central pixels:
     copied[1:-1, 1:-1] = (old[0:-2, 1:-1] + old[2:, 1:-1] + old[1:-1, 0:-2] + old[1:-1, 2:]) / 4
     #      ^^^^^^^^^^
-    #     Everything but the outermost pixels is considered here
+    #      Everything but the outermost pixels is considered here
 
     # For the edges and corners, there still need to be special cases.
     # Edges:
@@ -83,7 +83,7 @@ def do_iteration(init, field_size, electrode_offset, electrode_width, stop):
     return c_list, potential
 
 
-# This checks whether you executed this python script as a program (e.g. with `python 1_3.py`)
+# This checks whether you executed this Python script as a program (e.g. with `python 1_3.py`)
 # and is False when you imported it as a module from another script. It is not that useful
 # here, but may be in the future for projects with multiple files.
 if __name__ == '__main__':
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     electrode_width = 8
     # Stop criterion. The iteration is stopped when the maximum absolute difference between two
     # iteration steps is less than this
-    ε = 1e-4
+    ε = 1e-5
     c_list, potential = do_iteration(1, field_size, electrode_offset, electrode_width, ε)
 
     # Plot the evolution of the convergence
@@ -111,19 +111,19 @@ if __name__ == '__main__':
     # a 'divergent' colormap like RdBu or coolwarm should be used.
     plt.imshow(potential, origin='lower', cmap='RdBu')
     plt.title(f'{len(c_list)} iterations')
-    plt.colorbar(label='Potential in V')
+    plt.colorbar(label='Potential / V')
     # Add some contour lines (optional)
     CS = ax.contour(potential, colors='k', levels=np.arange(-750, 1000, 250))
     ax.clabel(CS, inline=1, fontsize=10, fmt='%1.0f')
-    plt.xlabel("x")
-    plt.ylabel("y")
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.tight_layout(pad=.5)
-    plt.savefig("build/1_3/potential.pdf")
+    plt.savefig('build/1_3/potential.pdf')
     plt.close()
 
     # Very, very optional:
     # It may be interesting to make a convergence analysis for different initial conditions.
-    # The following code does that. If you want to try it, uncomment it and line 4.
+    # The following code does that. If you want to try it, uncomment it and line 4+5.
     # Additionally you have to install the p-tqdm package (pip install --user p-tqdm), if
     # it is not already installed. The package provides the p_map command that allows us to
     # spread out the computation over multiple processes (and makes a nice progress bar!),
